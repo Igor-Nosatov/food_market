5896aec39bd724ff932a5d329c188312856897d0
Igor-Nosatov <sinevor13@gmail.com>
2020-02-28 13:33:32 +0200

    make basic model

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'price', 'image','category_id',
+    ];
+    
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
